f3000d34c35fda3bccff904e42cb77236e4a1646
saikumar <saik29253@gmail.com>
2024-07-06 22:08:20 +0530

    added sebnet name

--- practice/Assignment4.tf
+++ practice/Assignment4.tf
@@ -27,6 +27,9 @@ resource "aws_subnet" "my_public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  tags = {
+    Name = "ec2_public_subnet"
+  }
 }
 
 # Create an EC2 instance
