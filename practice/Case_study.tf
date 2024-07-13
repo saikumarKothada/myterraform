e995618f757a95f9fa1e18289bed907b5e6cff66
saikumar <saik29253@gmail.com>
2024-07-13 21:19:03 +0530

    s

--- practice/Case_study.tf
+++ practice/Case_study.tf
@@ -89,9 +89,6 @@ resource "aws_security_group" "websg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from all sources
-    tags = {
-        user = "terraform user"
-    }
   }
 }
 
