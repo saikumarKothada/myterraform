a8d688234b16d8d2cc60861d0192af0220a97a76
saikumar <saik29253@gmail.com>
2024-07-14 16:13:20 +0530

    ss

--- practice/Case_study.tf
+++ practice/Case_study.tf
@@ -85,8 +85,8 @@ resource "aws_security_group" "websg" {
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from all sources
   }
