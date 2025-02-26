815658c3ac9895df1ac0548d0aada4cbfa6171f0
saikumar <saikumarkothada@gmail.com>
2025-02-26 13:09:42 +0530

    added backend

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ terraform {
     required_providers{
         aws = {
             source = "hashicorp/aws"
-            version = "5.86.0"
+            version = "~>5.80.0"
         }
 
-        azure = {
+        azurerm = {
             source = "hashicorp/azurerm"
             version = "4.18.0"
         }
